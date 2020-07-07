4de6e807dc7e95fa7abae4121789f95202e64838
Trịnh Thái <trinhtrongthaipy@gmail.com>
2020-07-07 14:00:20 +0700

    commit

--- src/main/java/poly/com/api/NotificationAPI.java
+++ src/main/java/poly/com/api/NotificationAPI.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import poly.com.entity.Notification;
-import poly.com.request.PaginationRequest;
 import poly.com.service.NotificationService;
 
 @RestController
@@ -20,11 +19,11 @@ public class NotificationAPI {
 	NotificationService notificationService;
 	
 	@GetMapping
-	public ResponseEntity<Page<Notification>> pagenation(@RequestBody PaginationRequest paginationRequest){	
+	public ResponseEntity<Page<Notification>> pagenation(@RequestParam("page") int page, @RequestParam("size") int size,
+															@RequestParam("title") String title,
+															@RequestParam(name = "sortType", defaultValue = "DESC") String sortType ){	
 		
-		return notificationService.pagenation(paginationRequest);
+		return notificationService.pagenation(page, size, title, sortType);
 	}
-	
-	
 	
 }
