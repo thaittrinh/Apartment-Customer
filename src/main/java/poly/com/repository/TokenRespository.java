a30368b3ed96a62987d4ba51b037eb5195c3e04f
Trịnh Thái <trinhtrongthaipy@gmail.com>
2020-08-16 16:04:00 +0700

    complete email

--- src/main/java/poly/com/repository/TokenRespository.java
+++ src/main/java/poly/com/repository/TokenRespository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import poly.com.entity.TokenUser;
 
 import java.util.Date;
+import java.util.Optional;
 
 public interface TokenRespository extends JpaRepository<TokenUser, Integer> {
-    TokenUser findByToken(String token);
+    Optional<TokenUser> findByToken(String token);
 
     @Modifying
     @Query("delete from TokenUser t where t.expiryDate <= ?1")
--- src/main/java/poly/com/service/ChangePasswordService.java
+++ src/main/java/poly/com/service/ChangePasswordService.java
@@ -1,13 +1,11 @@
 package poly.com.service;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.ModelAndView;
+
 import poly.com.entity.Apartment;
 import poly.com.helper.MessageResponse;
 import poly.com.repository.ApartmentRepository;
